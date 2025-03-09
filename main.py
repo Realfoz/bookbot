4ccc63b57f1e5ad087e2ab8cafeae9c90385ed53
realfoz <scottpool@hotmail.com>
2025-03-09 15:03:54 +0000

    updated to include report and the ability to run a file path from the terminal

--- main.py
+++ main.py
@@ -3,15 +3,32 @@ def get_book_text(filepath): # takes filepath as input then outputs the contents
         return f.read()
     
 
-from stats import get_num_words
-from stats import letter_count
+from stats import get_num_words, letter_count, sort_dic
 
 def main():
-    book_path = "/home/scott/projects/github.com/bootdotdev/bookbot/books/frankenstein.txt"
+    import sys
+    if len(sys.argv) < 2:
+        print("Error: Please provide a path to a book file.")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("Error: Too many arguments provided. Only one book argument is accepted.")
+        sys.exit(1)
+    book_path = sys.argv[1] 
+    
     full_text = (get_book_text(book_path)) # runs the get_book_text func with the book path provided
     words = (get_num_words(full_text)) # uses the get_num_words func with the full_text to count the total words
     letter_counter = (letter_count(full_text)) # uses the letter_count func with full_text to count each unique char
-    print (F"{words} words found in the document")
-    print (letter_counter)
+    sorted_chars = sort_dic(letter_counter) # uses sorted_chars func to sort the dic values into reverse numerical order
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at {book_path}")
+    print ("----------- Word Count ----------")
+    print (f"Found {words} total words")
+    print ("--------- Character Count -------")
+    for i in sorted_chars:
+        character = i["char"] 
+        count = i["count"]  
+        print(f"{character}: {count}")
+    print ("============= END ===============")
+
       
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,5 @@
 def get_num_words(text):
-    num_words = text.split()
+    num_words = text.split() 
     return len(num_words)
 
 
@@ -8,10 +8,21 @@ def letter_count(text):
     letters = {}
     lower_case = text.lower()
     for char in lower_case:
-        if char in letters:
-            letters[char] += 1
-        else:
-            letters[char] = 1          
+        if char.isalpha():
+            if char in letters:
+                letters[char] += 1
+            else:
+                letters[char] = 1          
     return(letters)
 
- 
+
+def sort_dic(dict):
+    sorted_char_list = []
+    for char, count in dict.items():
+        char_dict = {"char": char, "count":count}
+        sorted_char_list.append(char_dict)
+        sorted_char_list.sort(key=lambda x: x["count"], reverse=True)
+    return sorted_char_list
+
+
+
