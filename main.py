0d5d36444a599152d37ca3421950f4c00d01f51a
realfoz <scottpool@hotmail.com>
2025-03-09 12:36:27 +0000

    added letter_count func to stats and into main

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ def get_book_text(filepath): # takes filepath as input then outputs the contents
     
 
 from stats import get_num_words
-
+from stats import letter_count
 
 def main():
     book_path = "/home/scott/projects/github.com/bootdotdev/bookbot/books/frankenstein.txt"
     full_text = (get_book_text(book_path)) # runs the get_book_text func with the book path provided
-    words = (get_num_words(full_text))
+    words = (get_num_words(full_text)) # uses the get_num_words func with the full_text to count the total words
+    letter_counter = (letter_count(full_text)) # uses the letter_count func with full_text to count each unique char
     print (F"{words} words found in the document")
+    print (letter_counter)
       
 main()
-
-
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def get_num_words(text):
     num_words = text.split()
     return len(num_words)
+
+
+
+def letter_count(text):
+    letters = {}
+    lower_case = text.lower()
+    for char in lower_case:
+        if char in letters:
+            letters[char] += 1
+        else:
+            letters[char] = 1          
+    return(letters)
+
+ 
